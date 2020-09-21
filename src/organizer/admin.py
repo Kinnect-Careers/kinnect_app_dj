b338ca911b70be360ae8c910ef88dd8ed48cb9a6
Rick Obi <sanunes.ricardo@gmail.com>
2020-09-21 16:32:17 -0400

    LINT | applied black

--- src/organizer/admin.py
+++ src/organizer/admin.py
@@ -1,27 +1,34 @@
 from django.contrib import admin
 
-from .models import (
-  Tag,
-  Company,
-  Partner
-)
+from .models import Tag, Company, Partner, Job
 
 
 @admin.register(Tag)
 class TagAdmin(admin.ModelAdmin):
-  list_display = ("name", "slug")
+    list_display = ("name", "slug")
+
 
 @admin.register(Company)
 class CompanyAdmin(admin.ModelAdmin):
-  list_display = ("name", "location", "slug")
+    list_display = ("name", "location", "slug")
+
 
 @admin.register(Partner)
 class PartnerAdmin(admin.ModelAdmin):
-  list_display = (
-    "name",
-    "location",
-    "contact",
-    "description",
-    "website",
-    "slug"
-  )
+    list_display = (
+        "name",
+        "location",
+        "contact",
+        "description",
+        "website",
+        "slug",
+    )
+
+
+@admin.register(Job)
+class JobAdmin(admin.ModelAdmin):
+    list_display = (
+        "title",
+        "pub_date",
+        "slug",
+    )
--- src/resume/admin.py
+++ src/resume/admin.py
@@ -1,92 +1,77 @@
 from django.contrib import admin
 
 from .models import (
-  Skill,
-  Experience,
-  Task,
-  Contact,
-  Institution,
-  Education,
-  Link,
-  Resume,
-  ApplicationSubmission
+    Skill,
+    Experience,
+    Task,
+    Contact,
+    Institution,
+    Education,
+    Link,
+    Resume,
+    ApplicationSubmission,
 )
+
 
 @admin.register(Skill)
 class SkillAdmin(admin.ModelAdmin):
-  list_display = (
-    "name",
-    "description",
-    "version",
-    "slug"
-  )
-  prepopulated_fields = {"slug": ("name",)}
+    list_display = (
+        "name",
+        "description",
+        "version",
+        "slug",
+    )
+    # prepopulated_fields = {"slug": ("name",)}
+
 
 @admin.register(Experience)
 class ExperienceAdmin(admin.ModelAdmin):
-  list_display = (
-    "company",
-    "started_at",
-    "ended_at",
-    "current",
-    "slug"
-  )
-  prepopulated_fields = {"slug": ("company.name", "started_at")}
+    list_display = (
+        "company",
+        "started_at",
+        "ended_at",
+        "current",
+        "slug",
+    )
+
 
 @admin.register(Contact)
 class ContactAdmin(admin.ModelAdmin):
-  list_display = (
-    "contact_type",
-    "contact",
-    "slug"
-  )
+    list_display = ("contact_type", "contact", "slug")
+
 
 @admin.register(Task)
 class TaskAdmin(admin.ModelAdmin):
-  list_display = (
-    "experience",
-    "description",
-    "slug"
-  )
+    list_display = ("experience", "description", "slug")
+
 
 @admin.register(Institution)
 class InstitutionAdmin(admin.ModelAdmin):
-  list_display = (
-    "name",
-    "location",
-    "slug"
-  )
+    list_display = ("name", "location", "slug")
+
 
 @admin.register(Education)
 class EducationAdmin(admin.ModelAdmin):
-  list_display = (
-    "degree_type",
-    "institution",
-    "started_at",
-    "ended_at",
-    "current",
-    "slug"
-  )
+    list_display = (
+        "degree_type",
+        "institution",
+        "started_at",
+        "ended_at",
+        "current",
+        "slug",
+    )
+
 
 @admin.register(Link)
 class LinkAdmin(admin.ModelAdmin):
-  list_display = (
-    "link_type",
-    "link",
-    "slug"
-  )
+    list_display = ("link_type", "link", "slug")
+
 
 @admin.register(Resume)
 class ResumeAdmin(admin.ModelAdmin):
-  list_display = (
-    "title",
-    "slug"
-  )
+    list_display = ("title", "slug")
+
 
 @admin.register(ApplicationSubmission)
 class ApplicationSubmissionAdmin(admin.ModelAdmin):
-  list_display = (
-    "resume",
-    "job",
-    "slug"
-  )
+    list_display = ("resume", "job", "slug")
